f1a743424dc4569edfa39329ddaa768a2b90b072
denlly <fan91163@gmail.com>
2018-07-05 10:51:08 +0800

    member   需要的字段

--- src/modules/member/member.service.ts
+++ src/modules/member/member.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMemberDto } from './dtos/main-member.dto';
+import { Member } from './member.entity';
+import { BaseService } from '../../common/base-class/base.service';
 
 @Injectable()
-export class MemberService {
+export class MemberService extends BaseService<MemberService> {
   private readonly members: CreateMemberDto[] = [];
+
+  constructor() {
+    super('MemberService');
+  }
 
   create(createMemberDto: CreateMemberDto) {
     this.members.push(createMemberDto);
   }
 
+  async findOneByToken(token: string): Promise<Member> {
+    return new Member();
+  }
 }
